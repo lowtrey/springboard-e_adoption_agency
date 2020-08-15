134f7820123982e6220e680d66e38a1dd290cd8b
Q. Price <priceqp@gmail.com>
2020-08-15 17:58:48 -0500

    configure app and models

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
 
 app = Flask(__name__)
 
+# Configure and Initialize Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+# Configure Debug Toolbar
+app.config['SECRET_KEY'] = "cookies"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
 @app.route("/")
 def show_home_route():
-  return "Home Route working..."
+  return render_template("pets.html")
