80280999a1bdae6ea1ae878fc91b7881146492bb
Q. Price <priceqp@gmail.com>
2020-08-17 12:41:17 -0500

    add species type validation

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, AnyOf
 
 class PetForm(FlaskForm):
 
   name = StringField("Pet Name", validators=[DataRequired()])
 
-  species = StringField("Species", validators=[DataRequired()])
+  species = StringField("Species", validators=[DataRequired(), AnyOf(["cat", "dog", "porcupine"], message="Invalid species. Must be 'cat', 'dog', or 'porcupine'")])
 
   photo_url = StringField("Photo URL", validators=[DataRequired(), URL()])
 
